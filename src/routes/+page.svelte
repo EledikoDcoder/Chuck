5c259b203b7100bbe07d08778b18394fcd9e28f6
EledikoDcoder <eledikofaruq@gmail.com>
2023-02-01 02:01:11 +0100

    changed color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,11 +31,11 @@
 
 <div class="flex flex-col w-[100vw] h-[100vh]  bg-gradient-to-r from-cyan-600 to-teal-500 justify-center items-center">
     <div class="flex flex-col w-[20rem] h-[23rem] shadow-lg bg-[#fff] rounded-md p-4">
-        <h1 class=" text-center font-bold text-3xl pb-[0.5rem] text-[1.5rem] text-cyan-600">CHUCK NORRIS</h1>
+        <h1 class=" text-center font-bold text-3xl pb-[0.5rem] text-[1.5rem] text-orange-500">CHUCK NORRIS</h1>
 
         <div class="flex flex-col gap-[2rem]  overflow-auto">
             {#each jokes as joke}
-            <ol class="flex text-1xl w-full pb-4 border-b border-cyan-600 text-cyan-600">
+            <ol class="flex text-1xl w-full pb-4 border-b border-orange-500 text-orange-500">
                 <li class="list-decimal">{joke.value}</li>
             </ol>
             {/each}
